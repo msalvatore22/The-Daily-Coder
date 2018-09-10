0c2f65f43e4f72134a8cdc6cbd94c8c5babea15c
Michael Salvatore <msalvatore22@gmail.com>
2018-09-10 16:39:12 -0400

    google oauth flow working with react/redux

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const App = () => {
-  return (
-    <div>
-      Hello world!
-    </div>
-  );
+import Header from './Header'
+
+const Dashboard = () => <h2>Dashboard</h2>
+const Landing = () => <h2>Landing</h2>
+
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/newsfeed" component={Dashboard} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 };
 
-export default App;
+export default connect(null, actions)(App);
---
+++ client/src/components/Header.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  renderContent(){
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li><a href="/auth/google">Login with google</a></li>
+        )
+      default:
+      return <li><a href="/api/logout">logout</a></li>
+    }
+  }
+  
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">
+            The Daily Coder
+          </a>
+          <ul className="right">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps({ auth }) {
+  return { auth }
+}
+
+export default connect(mapStateToProps)(Header);
